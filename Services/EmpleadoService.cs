424db25678fb6fc20fbd7e93bb5ac65c7a9e3cd8
Facundo Orellana <facundo.ore@hotmail.com>
2023-07-17 20:54:34 -0300

    metodos en empleadoservice

--- Services/EmpleadoService.cs
+++ Services/EmpleadoService.cs
@@ -1,6 +1,25 @@
-﻿namespace GestionCapitalHumano.Services
+﻿using GestionCapitalHumano.Models;
+
+namespace GestionCapitalHumano.Services
 {
     public class EmpleadoService
     {
+
+        private CapitalHumanoContext _context;
+
+        public EmpleadoService(CapitalHumanoContext capitalHumanoContext)
+        {
+            _context = capitalHumanoContext;
+        }
+
+        public Empleado crearEmpleado(Empleado empleado)
+        {
+            //new method
+            return _context.Empleados.Add(empleado);
+
+        }
+
+
+
     }
 }
